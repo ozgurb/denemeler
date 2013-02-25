83e53a62300c50092b559a42fa1e9c1b9d4ee273
ozgur b <ozgur.bbt@gmail.com>
2013-02-25 16:28:10 +0200

    Vector sınıfına bazı fonnksiyornalr eklendi

--- felanfilan.scala
+++ felanfilan.scala
@@ -1,5 +1,5 @@
 
-class Vect3(val x: Double, val y: Double, val z: Double) {
+class Vect3(var x: Double, val,r y: Double, var z: Double) {
 	def + (v: Vect3) = new Vect3(x + v.x, y + v.y, z + v.z)
 	def - (v: Vect3) = new Vect3(x - v.x, y - v.y, z - v.z)
 	def dot(v: Vect3) = x * v.x + y * v.y + z * v.z
@@ -9,5 +9,17 @@ class Vect3(val x: Double, val y: Double, val z: Double) {
         x * v.y - y * v.x  
 		)
 	def unary_- = new Vect3(-x, -y, -z)
+	def apply (index: Int) = index match {
+		case 0 => x
+		case 1 => y
+		case 2 => z
+	}
+	def update(index: Int, value: Double) {
+		index match {
+			case 0 => x = value
+			case 1 => y = value
+			case 2 => z = value
+		}
+	}
 	def printForm: String = "(" + x + ", " + y + ", " + z + ")"
 }
