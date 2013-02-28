1095da74e8cd0473e923ace77fe9a702b0504a6a
ali veli <ezopezop@gmail.com>
2013-02-28 14:07:16 +0200

    guncelllendi

--- felanfilan.scala
+++ felanfilan.scala
@@ -1,14 +1,14 @@
 
-class Vect3(var x: Double, val,r y: Double, var z: Double) {
-	def + (v: Vect3) = new Vect3(x + v.x, y + v.y, z + v.z)
-	def - (v: Vect3) = new Vect3(x - v.x, y - v.y, z - v.z)
+class Vect3(var x: Double, var y: Double, var z: Double) {
+	def + (v: Vect3) = Vect3(x + v.x, y + v.y, z + v.z)
+	def - (v: Vect3) = Vect3(x - v.x, y - v.y, z - v.z)
 	def dot(v: Vect3) = x * v.x + y * v.y + z * v.z
-	def cross(v: Vect3) = new Vect3(
+	def cross(v: Vect3) = Vect3(
         y * v.z - z * v.y,
         z * v.z - x * v.z,
         x * v.y - y * v.x  
 		)
-	def unary_- = new Vect3(-x, -y, -z)
+	def unary_- = Vect3(-x, -y, -z)
 	def apply (index: Int) = index match {
 		case 0 => x
 		case 1 => y
@@ -22,4 +22,9 @@ class Vect3(var x: Double, val,r y: Double, var z: Double) {
 		}
 	}
 	def printForm: String = "(" + x + ", " + y + ", " + z + ")"
+}
+
+
+object Vect3 {
+	def apply(x: Double, y: Double, z: Double) = new Vect3(x, y, z) 
 }
